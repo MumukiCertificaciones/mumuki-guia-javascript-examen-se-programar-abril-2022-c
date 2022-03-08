c6e24d121e0432fd6492426233af2c27e1861720
mayra@mumuki.org <mayra@mumuki.org>
2022-03-08 16:52:04 +0000

    Mumuki Export on 2022-03-08 16:52:04 +0000

--- 00005_Ejercicio 5/test..js
+++ 00005_Ejercicio 5/test..js
@@ -1,5 +1,13 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+describe("", function() {
+  it("Si hacemos $FUNCION([1,3,20,2]) nos retorna 26000", function() {
+    assert.equal($FUNCION([1,3,20,2]), 26000);
+  });
+  
+  it("Si hacemos $FUNCION([1,0,1,2]) nos retorna 4000", function() {
+    assert.equal($FUNCION([1,0,1,2]), 4000);
+  });
+  
+  it("Si hacemos $FUNCION([]) nos retorna 0", function() {
+    assert.equal($FUNCION([]), 0);
+  });
 })

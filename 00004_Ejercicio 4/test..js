b7630916d7150e09a4a15a9531d3e94de3ba2c5b
mayra@mumuki.org <mayra@mumuki.org>
2022-03-08 16:33:48 +0000

    Mumuki Export on 2022-03-08 16:33:48 +0000

--- 00004_Ejercicio 4/test..js
+++ 00004_Ejercicio 4/test..js
@@ -1,9 +1,14 @@
+function transform(texto) {
+  let  textoSinTildes = texto.toString().toLowerCase(texto).normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return textoSinTildes.replace('ano', 'año');
+}
+
 describe("", function() {
   it("Si hacemos $FUNCION(2020) nos retorna $STRING_NO_FIN", function() {
-    assert.equal($FUNCION(2020), "$STRING_NO_FIN");
+    assert.equal(transform($FUNCION(2020)),transform( "$STRING_NO_FIN"));
   });
   
   it("Si hacemos $FUNCION("+(Number($ANIO_CORTE - 1))+") nos retorna $STRING_FIN", function() {
-    assert.equal($FUNCION($ANIO_CORTE - 1), "$STRING_FIN");
+    assert.equal(transform($FUNCION($ANIO_CORTE - 1)), transform( "$STRING_FIN"));
   });
 })

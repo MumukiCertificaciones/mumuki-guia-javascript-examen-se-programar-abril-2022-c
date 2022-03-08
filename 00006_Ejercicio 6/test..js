db585829224b3b4574275d9e81c0a5f9afedf49e
mayra@mumuki.org <mayra@mumuki.org>
2022-03-08 18:01:36 +0000

    Mumuki Export on 2022-03-08 18:01:36 +0000

--- 00006_Ejercicio 6/test..js
+++ 00006_Ejercicio 6/test..js
@@ -3,10 +3,3 @@ describe("Descripción del grupo", function() {
     assert(true)
   })
 })
-
-fue descubierto por
-lo descubrió 
-
-
-$STRING_COMPOSICIÓN = El átomo/ El elemento
-$STRING_ANIO = fue descubierto en el año/ se descrubrió en el año

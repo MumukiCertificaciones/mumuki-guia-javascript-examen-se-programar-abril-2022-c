516ff40804b149a37c4e564f198347c7bd6d0ce1
mayra@mumuki.org <mayra@mumuki.org>
2022-03-08 18:07:04 +0000

    Mumuki Export on 2022-03-08 18:07:04 +0000

--- 00006_Ejercicio 6/test..js
+++ 00006_Ejercicio 6/test..js
@@ -1,5 +1,14 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+function transform(texto) {
+  let  textoSinTildes = texto.toString().toLowerCase(texto).normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return textoSinTildes.replace('ano', 'año');
+}
+
+describe("", function() {
+  it("Si hacemos $FUNCION(radio) nos retorna '$STRING_COMPOSICIÓN Radio $STRING_ANIO 1898 por Madame Curie'", function() {
+    assert.equal(transform($FUNCION(radio)), transform("$STRING_COMPOSICIÓN Radio $STRING_ANIO 1898 por Madame Curie"));
+  });
+  
+  it("Si hacemos $FUNCION(kripton) nos retorna '$STRING_COMPOSICIÓN Kriptón $STRING_ANIO 1898 por William Ramsay'", function() {
+    assert.equal(transform($FUNCION(casaDominguez)), transform("$STRING_COMPOSICIÓN Kriptón $STRING_ANIO 1898 por William Ramsay"));
+  });
 })

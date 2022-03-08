f4c4fb5d7fe4df46c668228bf27f724427d957ff
mayra@mumuki.org <mayra@mumuki.org>
2022-03-08 16:26:08 +0000

    Mumuki Export on 2022-03-08 16:26:08 +0000

--- 00004_Ejercicio 4/test..js
+++ 00004_Ejercicio 4/test..js
@@ -2,4 +2,8 @@ describe("", function() {
   it("Si hacemos $FUNCION(2020) nos retorna $STRING_NO_FIN", function() {
     assert.equal($FUNCION(2020), "$STRING_NO_FIN");
   });
+  
+  it("Si hacemos $FUNCION(Number($ANIO_CORTE - 1)) nos retorna $STRING_FIN", function() {
+    assert.equal($FUNCION($ANIO_CORTE - 1), "$STRING_FIN");
+  });
 })

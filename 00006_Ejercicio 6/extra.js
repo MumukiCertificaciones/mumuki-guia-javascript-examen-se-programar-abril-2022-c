dc79faa1e6516a37407c248a5be4a116f206984f
mayra@mumuki.org <mayra@mumuki.org>
2022-03-08 18:10:48 +0000

    Mumuki Export on 2022-03-08 18:10:48 +0000

---
+++ 00006_Ejercicio 6/extra.js
@@ -0,0 +1,11 @@
+let radio = {
+  elemento: "Radio",
+  anioDeDescubrimiento: 1898,
+  fueDescubiertoPor: "Madame Curie"
+}
+
+let kripton = {
+  elemento: "Kriptón",
+  anioDeDescubrimiento: 1898,
+  fueDescubiertoPor: "William Ramsay"
+}

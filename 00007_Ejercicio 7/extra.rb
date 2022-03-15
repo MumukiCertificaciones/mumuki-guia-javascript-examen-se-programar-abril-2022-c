a45de64b0a5f4cc0b08d77211f606e6a3856f432
mayra@mumuki.org <mayra@mumuki.org>
2022-03-15 19:14:10 +0000

    Mumuki Export on 2022-03-15 19:14:09 +0000

---
+++ 00007_Ejercicio 7/extra.rb
@@ -0,0 +1,12 @@
+module $OBJETO
+
+  def self.cantidad_de_crateres
+    @cantidad_de_crateres
+  end 
+  
+  def self.cantidad_de_crateres= (cantidad)
+    @cantidad_de_crateres = cantidad
+  end
+  
+end
+  

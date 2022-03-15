48b7f00509174a9ac9b5d17448af6e1783f507ab
mayra@mumuki.org <mayra@mumuki.org>
2022-03-15 14:38:24 +0000

    Mumuki Export on 2022-03-15 14:38:24 +0000

---
+++ 00008_Ejercicio 8/extra.rb
@@ -0,0 +1,47 @@
+module Tomate
+  def self.$PREDEFINIDO
+    true
+  end
+end
+
+module Berenjena
+  def self.$PREDEFINIDO
+    true
+  end
+end
+
+module Frutilla
+  def self.$PREDEFINIDO
+    true
+  end
+end
+
+module Frambuesa
+  def self.$PREDEFINIDO
+    false
+  end
+end
+
+module Palta
+  def self.$PREDEFINIDO
+    false
+  end
+end
+
+module Zapallo
+  def self.$PREDEFINIDO
+    true
+  end
+end
+
+module Papa
+  def self.$PREDEFINIDO
+    false
+  end
+end
+
+module Batata
+  def self.$PREDEFINIDO
+    true
+  end
+end

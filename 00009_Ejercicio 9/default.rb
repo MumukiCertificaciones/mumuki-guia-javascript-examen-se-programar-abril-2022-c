e660c1ce122bc2c3288267a10e2f1e94b53c68cc
gustavo@mumuki.org <gustavo@mumuki.org>
2022-03-15 18:49:15 +0000

    Mumuki Export on 2022-03-15 18:49:15 +0000

---
+++ 00009_Ejercicio 9/default.rb
@@ -0,0 +1,20 @@
+class Persona
+  def initialize(un_equipaje)
+    @equipaje = un_equipaje
+  end
+end
+
+class $CLASE_1
+  def initialize(unos_bolsillos)
+    @bolsillos = unos_bolsillos
+  end
+end
+
+class $CLASE_2
+end
+
+class Bolso
+  def initialize(un_peso)
+    @peso = un_peso
+  end
+end
